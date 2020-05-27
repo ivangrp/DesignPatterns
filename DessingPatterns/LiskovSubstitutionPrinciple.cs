851b561b73046dc82c8f596141ce183c1d8ff8ea
Ivan <ivangrp1992@gmail.com>
2020-05-27 17:36:53 -0500

    Finish Liskov Substitution Principle

---
+++ DessingPatterns/LiskovSubstitutionPrinciple.cs
@@ -0,0 +1,41 @@
+﻿namespace DessingPatterns
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+            
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Height = value; }
+        }
+
+        public override int Height
+        {
+            set { base.Width = base.Height = value; }
+        }
+    }
+}
--- DessingPatterns/Program.cs
+++ DessingPatterns/Program.cs
@@ -7,9 +7,14 @@
     {
         static void Main(string[] args)
         {
-            SingleResponsibilityMethod();
+            //SingleResponsibilityMethod();
+            //OpenClosedPrincipleMethod();
+            LiskovSubstitutionPrincipleMethod();
+
+            Console.ReadKey();
         }
 
+        #region SingleResponsibility
         public static void SingleResponsibilityMethod()
         {
             var j = new Journal();
@@ -22,5 +27,57 @@
             persistence.SaveToFile(j, fileName, true);
             Process.Start(fileName);
         }
+        #endregion
+
+        #region OpenClosedPrinciple
+        public static void OpenClosedPrincipleMethod()
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+                Console.WriteLine($" - {p.Name} is green");
+
+            var bf = new BetterFilter();
+            Console.WriteLine("Green products (new):");
+            foreach (var p in bf.Filter(products, new ColorSpecification(Color.Green)))
+                Console.WriteLine($" - {p.Name} is green");
+
+            Console.WriteLine("Large products");
+            foreach (var p in bf.Filter(products, new SizeSpecification(Size.Large)))
+                Console.WriteLine($" - {p.Name} is large");
+
+            Console.WriteLine("Large blue items");
+            foreach (var p in bf.Filter(products,
+                new AndSpecification<Product>(new ColorSpecification(Color.Blue), new SizeSpecification(Size.Large)))
+            )
+            {
+                Console.WriteLine($" - {p.Name} is big and blue");
+            }
+        }
+        #endregion
+
+        #region LiskovSubstitutionPrinciple
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
+        /// <summary>
+        /// El principio de sustitucion de liskov dice que se debe poder clasificar el elenco a un tipo base
+        /// y que la operacion deberia ser generalmente correcta
+        /// </summary>
+        public static void LiskovSubstitutionPrincipleMethod()
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            Console.WriteLine($"{rc} has area {Area(rc)}");
+
+            Square sq = new Square();
+            sq.Width = 4;
+            Console.WriteLine($"{sq} has area {Area(sq)}");
+        }
+        #endregion
     }
 }
